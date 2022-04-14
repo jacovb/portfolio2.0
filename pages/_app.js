0eb559692f353ec9766c68918bcb33a18af486ab
jacovb <vbiljon@gmail.com>
2022-04-14 14:47:30 +0100

    ditto mess

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,16 @@ function MyApp({ Component, pageProps }) {
       <Navbar />
       <div className="container mt-16 flex justify-between items-center mx-auto px-8 mx-auto md:px-14 lg:px-24 w-full">
         <NavDots />
+        {/* Portfolio */}
         <Component {...pageProps} />
+      </div>
+      <div className="container mt-64 flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full">
+        <section className="w-full">
+          <h2 id="portfolio" class="secondary-title">
+            Portfolio
+          </h2>
+          <p className="section-paragraph">Projects I've been working on</p>
+        </section>
       </div>
     </div>
   );
