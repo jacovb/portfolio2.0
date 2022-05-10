048daa9d1c932875adf0297d019f7134edad8f50
jacovb <vbiljon@gmail.com>
2022-05-10 06:37:50 +0100

    Project links to modals working

--- components/NavDots.jsx
+++ components/NavDots.jsx
@@ -4,16 +4,16 @@ export default function NavDots({ pageNum }) {
       <div className="flex flex-wrap md:flex-nowrap">
         <nav className="container h-screen inline-block lg:mr-24 lg:w-4 fixed left-percentage hidden lg:block">
           <div className="absolute left-50 top-50 transform -translate-y-1/2 space-y-6">
-            <a href="#home" className={`${(pageNum == "page1" ? "selected-circle" : "")} nav-dot block w-7 h-7 rounded-full border-4 border-nav bg-body`}>
+            <a href="#home" className={`${(pageNum == "page1" ? "selected-circle" : "")} nav-dot block w-7 h-7 rounded-full border-[3px] border-gray-900 bg-white`}>
               <span className="bg-black text-white px-2 py-1 rounded-md ml-10 opacity-0">Home</span>
             </a>
-            <a href="#about" className={`${(pageNum == "page2" ? "selected-circle" : "")} nav-dot block w-7 h-7 rounded-full border-4 border-nav bg-body`}>
+            <a href="#about" className={`${(pageNum == "page2" ? "selected-circle" : "")} nav-dot block w-7 h-7 rounded-full border-[3px] border-gray-900 bg-white`}>
               <span className="bg-black text-white px-2 py-1 rounded-md ml-10 opacity-0">About</span>
             </a>
-            <a href="#projects" className={`${(pageNum == "page3" ? "selected-circle" : "")} nav-dot block w-7 h-7 rounded-full border-4 border-nav bg-body`}>
+            <a href="#projects" className={`${(pageNum == "page3" ? "selected-circle" : "")} nav-dot block w-7 h-7 rounded-full border-[3px] border-gray-900 bg-white`}>
               <span className="bg-black text-white px-2 py-1 rounded-md ml-10 opacity-0">Projects</span>
             </a>
-            <a href="#contact" className={`${(pageNum == "page4" ? "selected-circle" : "")} nav-dot block w-7 h-7 rounded-full border-4 border-nav bg-body`}>
+            <a href="#contact" className={`${(pageNum == "page4" ? "selected-circle" : "")} nav-dot block w-7 h-7 rounded-full border-[3px] border-gray-900 bg-white`}>
               <span className="bg-black text-white px-2 py-1 rounded-md ml-10 opacity-0">Contact</span>
             </a>
           </div>
--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -1,7 +1,14 @@
 import Project from "./Project"
-import Link from "next/link";
+import { useState } from "react"
+import MortgageModal from "./MortgageModal";
+import TimesheetModal from "./TimesheetModal";
+import BarberModal from "./BarberModal";
 
 export default function Portfolio() {
+  const [isMortgageModalOpen, setIsMortgageModalOpen] = useState(false);
+  const [isTimesheetModalOpen, setIsTimesheetModalOpen] = useState(false);
+  const [isBarberModalOpen, setIsBarberModalOpen] = useState(false);
+
   return (
     <div className="container min-h-screen snap-start flex justify-between items-center mx-auto px-4 md:px-8 lg:px-12 w-full">
       <section id="projects" className="w-full">
@@ -10,23 +17,20 @@ export default function Portfolio() {
         </h2>
         <p className="section-paragraph">Projects I've been working on</p>
         <div className="flex flex-col items-center md:flex-row md:justify-between w-full ">
-          <Link href="/calculator">
-            <a>
-              <Project title="Mortgage Calculator" description="Calculate Monthly Payments" link="Demo" />
-            </a>
-          </Link>
-          <Link href="/timesheet">
-            <a>
-              <Project title="Punch-Card" description="Timesheet Harvester" link="Demo" />
-            </a>
-          </Link>
-          <Link href="/barbershop">
-            <a>
-              <Project title="The London Barber Co." description="Book and pay for your Barber appointments online" link="Demo" />
-            </a>
-          </Link>
+          <button onClick={() => setIsMortgageModalOpen(true)}>
+            <Project title="Mortgage Calculator" description="Calculate Monthly Payments" link="Demo" />
+          </button>
+          <button onClick={() => setIsTimesheetModalOpen(true)}>
+            <Project title="Punch-Card" description="Timesheet Harvester" link="Demo" />
+          </button>
+          <button onClick={() => setIsBarberModalOpen(true)}>
+            <Project title="The London Barber Co." description="Book and pay for your Barber appointments online" link="Demo" />
+          </button>
         </div>
       </section>
+      <MortgageModal isMortgageModalOpen={isMortgageModalOpen} setIsMortgageModalOpen={setIsMortgageModalOpen} />
+      <TimesheetModal isTimesheetModalOpen={isTimesheetModalOpen} setIsTimesheetModalOpen={setIsTimesheetModalOpen} />
+      <BarberModal isBarberModalOpen={isBarberModalOpen} setIsBarberModalOpen={setIsBarberModalOpen} />
     </div>
   )
 }
