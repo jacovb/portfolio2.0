2e8051977f116e91edb9bcd4d5d33d1b1fc076f9
jacovb <vbiljon@gmail.com>
2022-05-16 06:58:09 +0100

    project modal layout

--- components/MortgageModal.jsx
+++ components/MortgageModal.jsx
@@ -5,7 +5,7 @@ import PaymentCalculator from "../images/PaymentCalculator.gif"
 import Tooltip from './Tooltip'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { faEye, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faJs, faReact, faHtml5, faCss3} from "@fortawesome/free-brands-svg-icons";
 
@@ -43,26 +43,34 @@ export default function MortgageModal({isMortgageModalOpen, setIsMortgageModalOp
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full max-w-2xl transform overflow-hidden rounded-xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Panel className="relative w-full max-w-2xl transform overflow-hidden rounded-xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                   <Dialog.Title
                     as="h3"
                     className="text-lg w-full flex justify-center text-2xl font-bold leading-6 text-gray-900"
                   >
                     Mortgage Calculator
                   </Dialog.Title>
-                  <div className="mt-5">
-                    <Image src={PaymentCalculator} className="rounded" />
+                  
+                  <div className="mt-4">
+                    <p className="mx-6 md:mx-12 italic text-gray-600">
+                    Calculates and visualises Loan or Mortgage Payments. Graph and Payment Schedule show how Interest Rates and Initial Fixed Rate Periods impact your Oustanding Capital Amount
+                    </p>
                   </div>
+                  
+                  <div className="flex flex-col border-black border-2 rounded-lg mt-5">
+                    
+                    <Image src={PaymentCalculator} />
 
-                  <div className="mt-2 flex w-full justify-between">
-                    <a href="https://payment-calculator120.netlify.app/" target="_blank" className="w-full flex justify-center items-center bg-gray-300 mr-1.5 h-10 rounded cursor-pointer hover:bg-gray-400 focus:outline-none">
-                      <FontAwesomeIcon icon={faEye} className="h-5" />
-                      <div className="ml-2 text-lg font-semibold">DEMO</div>
-                    </a>
-                    <a href="https://github.com/jacovb/payment-calculator" target="_blank" className="w-full flex justify-center items-center bg-gray-300 ml-1.5 h-10 rounded cursor-pointer hover:bg-gray-400 focus:outline-none">
-                      <FontAwesomeIcon icon={faGithub} className="h-5" />
-                      <div className="ml-2 text-lg font-semibold">CODE</div>
-                    </a>
+                    <div className="flex w-full justify-between border-t-2 border-black divide-x-2 divide-black">
+                      <a href="https://payment-calculator120.netlify.app/" target="_blank" className="w-full flex justify-center items-center bg-gray-300 h-12 rounded-bl-md cursor-pointer hover:bg-gray-500 hover:text-white focus:outline-none transition ease-in-out">
+                        <FontAwesomeIcon icon={faEye} className="h-5" />
+                        <div className="ml-2 text-lg font-semibold">DEMO</div>
+                      </a>
+                      <a href="https://github.com/jacovb/payment-calculator" target="_blank" className="w-full flex justify-center items-center bg-gray-300 h-12 rounded-br-md cursor-pointer hover:bg-gray-500 hover:text-white focus:outline-none transition ease-in-out">
+                        <FontAwesomeIcon icon={faGithub} className="h-5" />
+                        <div className="ml-2 text-lg font-semibold">CODE</div>
+                      </a>
+                    </div>
                   </div>
                   
                   <div className="w-2/3 md:w-1/2 mx-auto h-10 mt-4 flex flex-row justify-around">
@@ -79,20 +87,14 @@ export default function MortgageModal({isMortgageModalOpen, setIsMortgageModalOp
                       <FontAwesomeIcon icon={faReact} style={{color: "#61DBFB"}} className="h-8"/>
                     </Tooltip>
                   </div>
-                  
-                  <div className="mt-4">
-                    <p>
-                    Calculates and vizualises Mortgage Payments. See how Interest Rates and Initial Fixed Rate Periods impact your Oustanding Capital Amount
-                    </p>
-                  </div>
 
-                  <div className="mt-4">
+                  <div className="absolute top-3 right-5">
                     <button
                       type="button"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      className="flex justify-center rounded-md border border-transparent p-3 text-gray-600 hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-gray-500 focus-visible:ring-offset-2"
                       onClick={closeMortgageModal}
                     >
-                      OK
+                      <FontAwesomeIcon icon={faXmark} />
                     </button>
                   </div>
                 </Dialog.Panel>
