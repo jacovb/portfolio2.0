dd543212bd4c9cae074cd1b5b132bb0bdc9d2ac5
jacovb <vbiljon@gmail.com>
2022-05-27 06:58:41 +0100

    playing around with blobs on hero page

--- components/About.jsx
+++ components/About.jsx
@@ -2,7 +2,7 @@ export default function About() {
   return (
     <div className="container min-h-screen snap-start flex justify-between items-center mx-auto px-4 md:px-8 lg:px-12 w-full">
       <section id="about" className="w-full">
-        <h2 className="before:content-[''] before:block before:w-12 before:h-2 before:bg-blue-700 before:rounded-full before:mb-1 text-3xl font-bold">
+        <h2 className="text-3xl font-bold">
           About
         </h2>
         <p className="text-gray-500 my-6 w-full max-w-3xl">Something about me</p>
