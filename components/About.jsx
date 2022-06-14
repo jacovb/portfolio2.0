1ed70ee149cf16fa5afc62c9d837a62ef72339c7
jacovb <vbiljon@gmail.com>
2022-06-14 07:09:08 +0100

    positioning blobs

--- components/About.jsx
+++ components/About.jsx
@@ -2,10 +2,15 @@ export default function About() {
   return (
     <div className="container min-h-screen snap-start flex justify-between items-center mx-auto px-4 md:px-8 lg:px-12 w-full">
       <section id="about" className="w-full">
-        <h2 className="text-3xl font-bold">
-          About
-        </h2>
-        <p className="text-gray-500 my-6 w-full max-w-3xl">Something about me</p>
+        <div className="relative flex">
+          <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" className="absolute h-40 -translate-x-10 -translate-y-14 z-0">
+            <path strokeWidth="3" stroke="#fecd00" fill="#ffd500" d="M38.7,-45.6C54.4,-41.8,74.4,-36.2,80.2,-24.6C86.1,-12.9,77.9,4.9,66.6,16C55.2,27.1,40.6,31.4,29,37.7C17.4,43.9,8.7,52,-1.3,53.8C-11.4,55.7,-22.7,51.3,-35.3,45.4C-47.9,39.5,-61.7,32,-66,21C-70.2,9.9,-64.9,-4.7,-58.8,-17.7C-52.7,-30.7,-45.7,-42.1,-35.8,-47.7C-25.9,-53.3,-12.9,-53.2,-0.7,-52.2C11.5,-51.2,23,-49.3,38.7,-45.6Z" transform="translate(100 100)" />
+          </svg>
+          <h1 className="text-3xl font-bold z-20">
+            About
+          </h1>
+        </div>
+        <p className="text-gray-500 my-10 w-full max-w-3xl z-10">Something about me</p>
       </section>
     </div>
   )
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
   return (
     <main id="home" className="container min-h-screen snap-start flex justify-between items-center mx-auto px-8 md:px-16 lg:px-24 w-full">
       <div className="relative flex flex-wrap lg:ml-10 lg:max-w-2xl justify-center md:justify-start max-w-sm md:max-w-lg mt-0 md:my-36">
-        <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" className="absolute h-54 w-full z-0 -translate-y-1/3 lg:-translate-y-1/3">
+        <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" className="h-[36rem] md:h-[30rem] lg:h-[32rem] absolute z-0 translate-x-12 md:-translate-x-16 lg:translate-x-4 -translate-y-48 md:-translate-y-36 lg:-translate-y-44">
           <path strokeWidth="1" stroke="#fecd00" fill="#ffd500" d="M26.1,-34.5C36,-34.3,47.6,-30.7,49.4,-23.5C51.2,-16.4,43.1,-5.7,43.9,8.1C44.8,21.9,54.5,38.8,50.4,43.9C46.4,49.1,28.4,42.5,16.4,37.9C4.3,33.2,-2,30.6,-16.2,35.4C-30.4,40.3,-52.7,52.6,-66.1,50.1C-79.5,47.5,-84.2,30.1,-82.4,14.4C-80.7,-1.3,-72.7,-15.4,-65.5,-29.3C-58.2,-43.2,-51.8,-56.9,-41,-56.7C-30.3,-56.4,-15.1,-42.2,-3.5,-36.7C8.1,-31.3,16.3,-34.6,26.1,-34.5Z" transform="translate(100 100)" />
         </svg>
         <h1 className="font-bold text-3xl md:text-4xl md:text-left mb-2 z-10">
