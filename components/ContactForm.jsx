19aaec50969d2bb5cfec20eab3b0f3292cddea11
jacovb <vbiljon@gmail.com>
2022-06-22 07:04:38 +0100

    added more contact details, but position not great yet

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -2,21 +2,21 @@ export default function ContactForm() {
   return (
     <form name="contact" method="POST" data-netlify="true" className="flex justify-center items-center flex-col w-full">
       <input type="hidden" name="form-name" value="contact" />
-      <p className="w-96 h-20">
+      <p className="w-96 h-18">
         <label htmlFor="name" className="pl-4 text-base text-gray-500">
           Name
         </label> 
         <br />
-        <input type="text" name="name" id="name" className="w-full h-12 py-auto px-4 border-2 border-black rounded-lg focus:bg-gray-100 hover:drop-shadow-xl focus:drop-shadow-xl"/>
+        <input type="text" name="name" id="name" className="w-full py-2 px-4 border-2 border-black rounded-lg focus:bg-gray-100 hover:drop-shadow-xl focus:drop-shadow-xl"/>
       </p>
-      <p className="w-96 h-20">
+      <p className="w-96 h-18 mt-1">
         <label htmlFor="email" className="pl-4 text-base text-gray-500">
           Email
         </label> 
         <br />
-        <input type="email" name="email" id="email" className="w-full h-12 py-auto px-4 border-2 border-black rounded-lg focus:bg-gray-100 hover:drop-shadow-xl focus:drop-shadow-xl"/>
+        <input type="email" name="email" id="email" className="w-full py-2 px-4 border-2 border-black rounded-lg focus:bg-gray-100 hover:drop-shadow-xl focus:drop-shadow-xl"/>
       </p>
-      <p className="w-96 h-48">
+      <p className="w-96 h-48 mt-1">
         <label htmlFor="message" className="pl-4 text-base text-gray-500">
           Message
         </label> <br />
